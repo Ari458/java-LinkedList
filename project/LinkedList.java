f5062a9c5431da1c2a4649028378336f09bc6620
Ari458 <44812154+Ari458@users.noreply.github.com>
2022-05-15 13:05:57 +0530

    Add files via upload

---
+++ project/LinkedList.java
@@ -0,0 +1,12 @@
+public class LinkedList{
+    public static void main(String[] args) throws Exception{
+        Node root=null;
+        root=NodeOperations.addEnd(root,"Aridam Dutta");
+        root=NodeOperations.addEnd(root,"Sohini Debnath");
+        root=NodeOperations.addEnd(root,"Ishani Deb");
+        root=NodeOperations.addEnd(root,189.90);
+        root=NodeOperations.addEnd(root,12);
+        root=NodeOperations.delSpecificPos(root,3);
+        NodeOperations.show(root);
+    }
+}
